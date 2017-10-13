5d4ef022f1084ac82b290b70dee906b9880961eb
felipemcardoso <felipe.computacao@gmail.com>
2017-10-13 09:35:06 -0300

    add Websocket template

---
+++ src/main/java/br/com/felipemcardoso/controller/WebSocketController.java
@@ -0,0 +1,22 @@
+package br.com.felipemcardoso.controller;
+
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/notification")
+public class WebSocketController {
+
+    @OnOpen
+    public void onOpen(Session session) {
+        System.out.println(session.getId());
+    }
+
+    @OnMessage
+    public String onMessage(String message) {
+        System.out.println(message);
+
+        return message + " received";
+    }
+}
