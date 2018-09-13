c6aa7918df1c9a6cc330d2cb59acf7d7b4763342
felipemcardoso <felipe.computacao@gmail.com>
2018-09-12 22:10:23 -0300

    Added sf4j

--- src/main/java/br/com/felipemcardoso/application/MyServiceImpl.java
+++ src/main/java/br/com/felipemcardoso/application/MyServiceImpl.java
@@ -1,7 +1,8 @@
 package br.com.felipemcardoso.application;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Singleton;
 import javax.transaction.Transactional;
@@ -11,7 +12,7 @@ import java.util.List;
 @Singleton
 public class MyServiceImpl implements MyService {
 
-    private static Logger logger = LogManager.getLogger(MyServiceImpl.class);
+    private static Logger logger = LoggerFactory.getLogger(MyService.class);
 
     public MyServiceImpl() {
     }
